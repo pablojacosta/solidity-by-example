9d7adf99d9c1a6294c696af3fff53a6a31e4d484
Pablo Acosta <pablo.acosta@nftstudios.io>
2024-04-17 15:38:41 -0300

    feat: :sparkles: ERC20 contracts.

---
+++ contracts/Applications/ERC20/IERC20.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+/*
+ERC20 tokens provide functionalities to
+
+transfer tokens
+allow others to transfer tokens on behalf of the token holder
+* */
+
+interface IERC20 {
+    function totalSupply() external view returns (uint256);
+
+    function balanceOf(address account) external view returns (uint256);
+
+    function transfer(
+        address recipient,
+        uint256 amount
+    ) external returns (bool);
+
+    function allowance(
+        address owner,
+        address spender
+    ) external view returns (uint256);
+
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) external returns (bool);
+}
