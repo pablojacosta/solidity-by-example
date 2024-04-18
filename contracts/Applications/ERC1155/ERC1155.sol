ae4ce812a0ee383fa9f8ad279ae1a8bef7cd9057
Pablo Acosta <pablo.acosta@nftstudios.io>
2024-04-18 13:34:47 -0300

    feat: :sparkles: Multitoken refactor.

--- contracts/Applications/ERC1155/ERC1155.sol
+++ contracts/Applications/ERC1155/ERC1155.sol
@@ -258,28 +258,3 @@ contract ERC1155 is IERC1155 {
         emit TransferBatch(msg.sender, from, address(0), ids, values);
     }
 }
-
-contract MyMultiToken is ERC1155 {
-    function mint(uint256 id, uint256 value, bytes memory data) external {
-        _mint(msg.sender, id, value, data);
-    }
-
-    function batchMint(
-        uint256[] calldata ids,
-        uint256[] calldata values,
-        bytes calldata data
-    ) external {
-        _batchMint(msg.sender, ids, values, data);
-    }
-
-    function burn(uint256 id, uint256 value) external {
-        _burn(msg.sender, id, value);
-    }
-
-    function batchBurn(
-        uint256[] calldata ids,
-        uint256[] calldata values
-    ) external {
-        _batchBurn(msg.sender, ids, values);
-    }
-}
